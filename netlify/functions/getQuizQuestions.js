83dd738eeaefd05998ea9e53667aea13a0c4159e
Nika Skeba <67114318+nikaskeba@users.noreply.github.com>
2023-08-25 13:40:52 -0700

    move api call to serverside

--- netlify/functions/getQuizQuestions.js
+++ netlify/functions/getQuizQuestions.js
@@ -1,24 +1,32 @@
-// getQuestions.js
 const axios = require('axios');
 
 exports.handler = async function(event, context) {
-    const body = JSON.parse(event.body);
-    const response = await axios.post('https://api.openai.com/v1/engines/davinci/completions', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
-        },
-        body: JSON.stringify({
-            prompt: body.prompt,
-            max_tokens: 150
-        })
-    });
-    
-    const data = response.data;
+    try {
+        const response = await axios.post('https://api.openai.com/v1/chat/completions', {
+            model: "gpt-4-0314",
+            messages: [
+                {"role": "system", "content": "You are a Spanish teacher"},
+                {"role": "user", "content": "Generate quiz questions for Spanish subjunctive leaving the verb in its unconjugated form. List the answers at the end."}
+            ],
+            max_tokens: 120,
+            stop: null,
+            temperature: 0.7,
+        }, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
+            }
+        });
 
-    return {
-        statusCode: 200,
-        body: JSON.stringify(data.choices[0].text.trim())
-    };
+        return {
+            statusCode: 200,
+            body: JSON.stringify(response.data)
+        };
+    } catch (error) {
+        console.error("Error making API call:", error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: "Failed fetching quiz data." })
+        };
+    }
 };
--- src/QuizComponent.js
+++ src/QuizComponent.js
@@ -4,21 +4,11 @@ const QuizComponent = () => {
   const [quizData, setQuizData] = useState(null);
   const [loading, setLoading] = useState(false);
 
-  const generateNewQuiz = async () => {
+const generateNewQuiz = async () => {
     setLoading(true);
     try {
       const response = await fetch('/.netlify/functions/getQuizQuestions', {
         method: 'POST',
-        body: JSON.stringify({
-          model: "gpt-4-0314",
-          messages: [
-            {"role": "system", "content": "You are a Spanish teacher"},
-            {"role": "user", "content": "Generate quiz questions for Spanish subjunctive leaving the verb in its unconjugated form. List the answers at the end."}
-          ],
-          max_tokens: 120,
-          stop: null,
-          temperature: 0.7,
-        }),
         headers: {
           'Content-Type': 'application/json',
         },
@@ -31,7 +21,8 @@ const QuizComponent = () => {
       console.error("Error fetching quiz data:", error);
       setLoading(false);
     }
-  };
+};
+
 
   return (
     <div>
