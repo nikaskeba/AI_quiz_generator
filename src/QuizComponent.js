6a42f2e419853a6a2291fa672d93b5bd890b7f74
Nika Skeba <67114318+nikaskeba@users.noreply.github.com>
2023-08-25 13:00:49 -0700

    debugging

--- src/QuizComponent.js
+++ src/QuizComponent.js
@@ -1,18 +1,18 @@
-import React, { useEffect, useState } from 'react';
-let userAnswer; 
+import React, { useState } from 'react';
 
+const QuizComponent = () => {
+  const [quizData, setQuizData] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-const QuizGenerator = () => {
-  const [questions, setQuestions] = useState([]);
-  
-useEffect(() => {
-    const fetchQuizQuestions = async () => {
-      const response = await fetch('https://api.openai.com/v1/chat/completions', {
+  const generateNewQuiz = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch('/.netlify/functions/getQuizQuestions', {
         method: 'POST',
         body: JSON.stringify({
           model: "gpt-4-0314",
           messages: [
-            {"role": "system", "content": "You are a spanish teacher"},
+            {"role": "system", "content": "You are a Spanish teacher"},
             {"role": "user", "content": "Generate quiz questions for Spanish subjunctive leaving the verb in its unconjugated form. List the answers at the end."}
           ],
           max_tokens: 120,
@@ -21,40 +21,30 @@ useEffect(() => {
         }),
         headers: {
           'Content-Type': 'application/json',
-          'Authorization': `Bearer ${process.env.OPENAI_API_KEY}` // replace with your API key or use environment variables for security
         },
       });
+
       const data = await response.json();
-      // Depending on the API's response format, you might need to adjust how you extract the questions
-      setQuestions(data.choices[0].message.content.split('\n')); // Example way to split by newline
-    };
-
-    fetchQuizQuestions();
-}, []);
-
-const handleSubmission = (questionIndex, userAnswer) => {
-    const correctAnswer = questions[questionIndex].answer;
-    if (userAnswer === correctAnswer) {
-        alert("Correct!");
-    } else {
-        alert("Wrong!");
+      setQuizData(data);
+      setLoading(false);
+    } catch (error) {
+      console.error("Error fetching quiz data:", error);
+      setLoading(false);
     }
-};
+  };
 
-return (
+  return (
     <div>
-      {questions.map((question, index) => (
-        <div key={index}>
-          <p>{question.sentence}</p>
-          <input 
-            type="text" 
-            onChange={e => userAnswer = e.target.value} 
-          />
-          <button onClick={() => handleSubmission(index, userAnswer)}>Submit</button>
-        </div>
-      ))}
+      <button onClick={generateNewQuiz}>Generate New Quiz</button>
+      
+      {loading && <p>Loading...</p>}
+
+      {/* Display API Response for Debugging */}
+      {quizData && (
+        <pre>{JSON.stringify(quizData, null, 2)}</pre>
+      )}
     </div>
   );
 };
 
-export default QuizGenerator;
+export default QuizComponent;
