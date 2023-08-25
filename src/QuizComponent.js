94c608f32eefb23a9c412a76d279af7e7194f2ff
Nika Skeba <67114318+nikaskeba@users.noreply.github.com>
2023-08-25 16:18:12 -0700

    Update QuizComponent.js

--- src/QuizComponent.js
+++ src/QuizComponent.js
@@ -5,8 +5,6 @@ const QuizComponent = () => {
   const [quizData, setQuizData] = useState(null);
   const [loading, setLoading] = useState(false);
   const [showAnswers, setShowAnswers] = useState(false);  // New state variable
-   const [userAnswers, setUserAnswers] = useState({}); // New state to hold user answers
-
   const [feedback, setFeedback] = useState({}); // New state to hold feedback for each answer
 
 const generateNewQuiz = async () => {
@@ -29,33 +27,13 @@ const generateNewQuiz = async () => {
 
 };
 
-  const handleInputChange = (index, event) => {
-    setUserAnswers(prev => ({ ...prev, [index]: event.target.value }));
-  };
-
-  const handleSubmit = (index, correctAnswer) => {
-    if (userAnswers[index] === correctAnswer) {
-      setFeedback(prev => ({ ...prev, [index]: 'correct' }));
-    } else {
-      setFeedback(prev => ({ ...prev, [index]: 'incorrect' }));
-    }
-  };
-
   const formatQuestions = (data) => {
     if (data && data.choices && data.choices[0] && data.choices[0].message) {
       const sentences = data.choices[0].message.content.split('\n');
       return sentences.map((sentence, index) => {
         if (index < 5) {
-          const match = sentence.match(/\((\w+)\)/);
-          const correctAnswer = match ? match[1] : null;
-          const formattedSentence = sentence.replace(/\((\w+)\)/g, (match, word) => 
-            `(${word}) <input 
-                          style={{ borderColor: feedback[index] === 'correct' ? 'green' : feedback[index] === 'incorrect' ? 'red' : '' }} 
-                          onChange={(e) => handleInputChange(index, e)} 
-                          placeholder="${word}" 
-                       /> 
-                       <button onClick={() => handleSubmit(index, correctAnswer)}>Submit</button>`
-          );
+          // Replace words in parentheses with word + input box
+          const formattedSentence = sentence.replace(/\((\w+)\)/g, '($1) <input placeholder="$1" />');
           return (
             <p key={index} dangerouslySetInnerHTML={{ __html: formattedSentence }} />
           );
