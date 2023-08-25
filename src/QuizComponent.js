bb05a69b7f68d2ac23a39230d64cb5fb59fcf3e7
Nika Skeba <67114318+nikaskeba@users.noreply.github.com>
2023-08-25 16:11:59 -0700

    Update QuizComponent.js

--- src/QuizComponent.js
+++ src/QuizComponent.js
@@ -5,6 +5,7 @@ const QuizComponent = () => {
   const [quizData, setQuizData] = useState(null);
   const [loading, setLoading] = useState(false);
   const [showAnswers, setShowAnswers] = useState(false);  // New state variable
+  const [feedback, setFeedback] = useState({}); // New state to hold feedback for each answer
 
 const generateNewQuiz = async () => {
     setLoading(true);
@@ -26,13 +27,33 @@ const generateNewQuiz = async () => {
 
 };
 
+  const handleInputChange = (index, event) => {
+    setUserAnswers(prev => ({ ...prev, [index]: event.target.value }));
+  };
+
+  const handleSubmit = (index, correctAnswer) => {
+    if (userAnswers[index] === correctAnswer) {
+      setFeedback(prev => ({ ...prev, [index]: 'correct' }));
+    } else {
+      setFeedback(prev => ({ ...prev, [index]: 'incorrect' }));
+    }
+  };
+
   const formatQuestions = (data) => {
     if (data && data.choices && data.choices[0] && data.choices[0].message) {
       const sentences = data.choices[0].message.content.split('\n');
       return sentences.map((sentence, index) => {
         if (index < 5) {
-          // Replace words in parentheses with word + input box
-          const formattedSentence = sentence.replace(/\((\w+)\)/g, '($1) <input placeholder="$1" />');
+          const match = sentence.match(/\((\w+)\)/);
+          const correctAnswer = match ? match[1] : null;
+          const formattedSentence = sentence.replace(/\((\w+)\)/g, (match, word) => 
+            `(${word}) <input 
+                          style={{ borderColor: feedback[index] === 'correct' ? 'green' : feedback[index] === 'incorrect' ? 'red' : '' }} 
+                          onChange={(e) => handleInputChange(index, e)} 
+                          placeholder="${word}" 
+                       /> 
+                       <button onClick={() => handleSubmit(index, correctAnswer)}>Submit</button>`
+          );
           return (
             <p key={index} dangerouslySetInnerHTML={{ __html: formattedSentence }} />
           );
