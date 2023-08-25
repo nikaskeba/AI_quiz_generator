d1ba20fed0635f7cee8ba29208cabd2d2760443f
Nika Skeba <67114318+nikaskeba@users.noreply.github.com>
2023-08-25 12:47:30 -0700

    fix open ai call to use gpt4

--- src/QuizComponent.js
+++ src/QuizComponent.js
@@ -4,25 +4,34 @@ let userAnswer;
 
 const QuizGenerator = () => {
   const [questions, setQuestions] = useState([]);
-
-  useEffect(() => {
+  
+useEffect(() => {
     const fetchQuizQuestions = async () => {
-      const response = await fetch('https://api.chatgpt.com/quiz-questions', {
+      const response = await fetch('https://api.openai.com/v1/chat/completions', {
         method: 'POST',
         body: JSON.stringify({
-          prompt: 'Generate quiz questions for Spanish subjunctive leaving the verb in its unconjugated form. List the answers at the end.',
+          model: "gpt-4-0314",
+          messages: [
+            {"role": "system", "content": "You are a spanish teacher"},
+            {"role": "user", "content": "Generate quiz questions for Spanish subjunctive leaving the verb in its unconjugated form. List the answers at the end."}
+          ],
+          max_tokens: 120,
+          stop: null,
+          temperature: 0.7,
         }),
         headers: {
           'Content-Type': 'application/json',
-          // Add any necessary authentication headers
+          'Authorization': 'Bearer OPENAI_API_KEY' // replace with your API key or use environment variables for security
         },
       });
       const data = await response.json();
-      setQuestions(data.questions);
+      // Depending on the API's response format, you might need to adjust how you extract the questions
+      setQuestions(data.choices[0].message.content.split('\n')); // Example way to split by newline
     };
 
     fetchQuizQuestions();
-  }, []);
+}, []);
+
 const handleSubmission = (questionIndex, userAnswer) => {
     const correctAnswer = questions[questionIndex].answer;
     if (userAnswer === correctAnswer) {
