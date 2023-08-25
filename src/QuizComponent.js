6549ff09089f35c6e0c01f7bd0a6919f2e9da909
Nika Skeba <67114318+nikaskeba@users.noreply.github.com>
2023-08-25 14:35:27 -0700

    Update QuizComponent.js

--- src/QuizComponent.js
+++ src/QuizComponent.js
@@ -2,41 +2,69 @@
 import React, { useState } from 'react';
 
 const QuizComponent = () => {
-  const [quizData, setQuizData] = useState(null);
-  const [loading, setLoading] = useState(false);
+    const [quizData, setQuizData] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [userAnswers, setUserAnswers] = useState([]);
 
-const generateNewQuiz = async () => {
-    setLoading(true);
-    try {
-      const response = await fetch('/.netlify/functions/getQuizQuestions', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
+    const generateNewQuiz = async () => {
+        setLoading(true);
+        try {
+            const response = await fetch('/.netlify/functions/getQuizQuestions', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
 
-      const data = await response.json();
-      setQuizData(data);
-      setLoading(false);
-    } catch (error) {
-      console.error("Error fetching quiz data:", error);
-      setLoading(false);
-    }
-};
+            const data = await response.json();
+            const { choices } = data;
+            const parsedData = parseGpt3Output(choices[0].message.content);
+            setQuizData(parsedData.questions);
+            setUserAnswers(Array(parsedData.questions.length).fill(""));
+            setLoading(false);
+        } catch (error) {
+            console.error("Error fetching quiz data:", error);
+            setLoading(false);
+        }
+    };
 
+    const parseGpt3Output = (output) => {
+        const [questionsText, answersText] = output.split("Answers:");
+        const questions = questionsText.trim().split("\n");
+        const answers = answersText.trim().split("\n");
 
-  return (
-    <div>
-      <button onClick={generateNewQuiz}>Generate New Quiz</button>
-      
-      {loading && <p>Loading...</p>}
+        return { questions, answers };
+    };
 
-      {/* Display API Response for Debugging */}
-      {quizData && (
-        <pre>{JSON.stringify(quizData, null, 2)}</pre>
-      )}
-    </div>
-  );
+    const handleInputChange = (index, event) => {
+        const newAnswers = [...userAnswers];
+        newAnswers[index] = event.target.value;
+        setUserAnswers(newAnswers);
+    };
+
+    const checkAnswer = (index) => {
+        const { answers } = parseGpt3Output(quizData);
+        if (userAnswers[index] === answers[index]) {
+            alert('Correct!');
+        } else {
+            alert('Wrong answer. Try again.');
+        }
+    };
+
+    return (
+        <div>
+            <button onClick={generateNewQuiz}>Generate New Quiz</button>
+
+            {loading && <p>Loading...</p>}
+
+            {quizData && quizData.map((question, index) => (
+                <div key={index}>
+                    <p dangerouslySetInnerHTML={{ __html: question.replace("_______", `<input type="text" onChange={(e) => handleInputChange(${index}, e)} />`) }}></p>
+                    <button onClick={() => checkAnswer(index)}>Submit</button>
+                </div>
+            ))}
+        </div>
+    );
 };
 
 export default QuizComponent;
