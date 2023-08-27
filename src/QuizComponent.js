4948cb083166803a3707941f7165272c2fd3fce7
Nika Skeba <67114318+nikaskeba@users.noreply.github.com>
2023-08-26 23:07:59 -0700

    add imperative

--- src/QuizComponent.js
+++ src/QuizComponent.js
@@ -24,14 +24,14 @@ const generateNewQuiz = async () => {
 
  // Determine the content based on the selected quiz type
     let userContent;
-    if (quizType === 'Subjunctive') {
-      userContent = "Generate a Spanish quiz that numerically lists 5 unique Spanish subjunctive sentences. In each sentence, leave the verb without conjugation and display the verb within (). Keep the 5 generated sentences together. Write the word solution and then list the 5 conjugated verb solutions, listing only the verb, in numerical order after the questions. List only the questions and solutions with no other text.";
-    } else if (quizType === 'Basic Conjugation') {
-      userContent = "Generate a Spanish quiz that numerically lists 5 unique Spanish present tense sentences. In each sentence, leave the verb without conjugation and display the verb within (). Keep the 5 generated sentences together. Write the word solution and then list the 5 conjugated verbs solution, writing only the conjugated verb and no other text in numerical order after the questions. List only the questions and solutions with no other text.";
-    }
- } else if (quizType === 'Imperative') {
-      userContent = "Generate a Spanish quiz that numerically lists 5 unique Spanish imperative sentences. In each sentence, leave the verb without conjugation and display the verb within (). Keep the 5 generated sentences together. Write the word solution and then list the 5 conjugated verbs solution, writing only the conjugated verb and no other text in numerical order after the questions. List only the questions and solutions with no other text.";
-    }
+      if (quizType === 'Imperative') {
+    userContent = "Generate a Spanish quiz that numerically lists 5 unique Spanish imperative sentences. In each sentence, leave the verb without conjugation and display the verb within (). Keep the 5 generated sentences together. Write the word solution and then list the 5 conjugated verb solutions, listing only the verb, in numerical order after the questions. List only the questions and solutions with no other text.";
+} else if (quizType === 'Subjunctive') {
+    userContent = "Generate a Spanish quiz that numerically lists 5 unique Spanish subjunctive sentences. In each sentence, leave the verb without conjugation and display the verb within (). Keep the 5 generated sentences together. Write the word solution and then list the 5 conjugated verb solutions, listing only the verb, in numerical order after the questions. List only the questions and solutions with no other text.";
+} else if (quizType === 'Basic Conjugation') {
+    userContent = "Generate a Spanish quiz that numerically lists 5 unique Spanish present tense sentences. In each sentence, leave the verb without conjugation and display the verb within (). Keep the 5 generated sentences together. Write the word solution and then list the 5 conjugated verbs solution, writing only the conjugated verb and no other text in numerical order after the questions. List only the questions and solutions with no other text.";
+}
+
     try {
       const response = await fetch('/.netlify/functions/getQuizQuestions', {
         method: 'POST',
@@ -146,7 +146,7 @@ const formatQuestions = (data) => {
     setQuizType(type);
   };
 
-  return (
+return (
     <div>
       {/* Selector buttons for quiz type */}
       <div className="quiz-selector">
@@ -162,11 +162,12 @@ const formatQuestions = (data) => {
         >
             Basic Conjugation
         </button>
-              <button 
+        {/* New button for Imperative */}
+        <button 
             className={quizType === 'Imperative' ? 'selected-quiz' : ''} 
             onClick={() => selectQuizType('Imperative')}
         >
-           Imperative
+            Imperative
         </button>
       </div>
 
@@ -178,6 +179,7 @@ const formatQuestions = (data) => {
 
       {loading && <p>Loading...</p>}
     </div>
-);
+  );
 };
+
 export default QuizComponent;
