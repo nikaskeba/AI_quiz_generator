e0f635a409acb011cae74b409f717bf848fac4d0
Nika Skeba <67114318+nikaskeba@users.noreply.github.com>
2023-08-29 00:17:02 -0700

    change quiz generation method

--- src/QuizComponent.js
+++ src/QuizComponent.js
@@ -1,6 +1,10 @@
 //QuizComponent.js
 import React, { useState } from 'react';
 import './QuizComponent.css';
+import axios from 'axios';
+const pattern_with_prefix = /(.*?)\(([^()]+)\)([^()]+)\(([^()]+)\)/;
+
+
 const QuizComponent = () => {
   const [quizData, setQuizData] = useState(null);
   const [loading, setLoading] = useState(false);
@@ -25,28 +29,43 @@ const generateNewQuiz = async () => {
  // Determine the content based on the selected quiz type
     let userContent;
       if (quizType === 'Imperative') {
-    userContent = "Generate a Spanish quiz that numerically lists 5 unique Spanish imperative sentences. In each sentence, leave the verb without conjugation and display the verb within (). Keep the 5 generated sentences together. Write the word solution and then conjugate and list the 5 one word only conjugated verb solutions, listing only the verb, in numerical order after the questions. List only the questions and solutions with no other text.";
+    userContent = "generate 5 complete spanish imperative sentences with the sentence verb unconjugated inside a () and the conjugated answer at the end in () for example '1.(venir) a la fiesta. (venga)'  output only sentences 1 to 5";
 } else if (quizType === 'Subjunctive') {
-    userContent = "Generate a Spanish quiz that numerically lists 5 unique Spanish subjunctive sentences. In each sentence, leave the verb without conjugation and display the verb within (). Keep the 5 generated sentences together. Write the word solution and then conjugate and list the 5 one word only conjugated verb solutions, listing only the verb, in numerical order after the questions. List only the questions and solutions with no other text.";
+    userContent = "generate 5 complete spanish subjunctive sentences with the sentence verb unconjugated inside a () and the conjugated answer at the end in () for example '1. Es probable que Juan (venir) a la fiesta. (venga)'  output only sentences 1 to 5";
 } else if (quizType === 'Basic Conjugation') {
-    userContent = "Generate a Spanish quiz that numerically lists 5 unique Spanish present tense sentences. In each sentence, leave the verb without conjugation and display the verb within (). Keep the 5 generated sentences together. Write the word solution and then conjugate and list the 5 one word only conjugated verbs solutions writing only the conjugated verb and no other text in numerical order after the questions. List only the questions and solutions with no other text.";
+    userContent = "generate 5 basic spanish present tense sentences with the sentence verb unconjugated inside a () and the conjugated answer at the end in () for example '1. Juan (venir) a la fiesta. (viene)'  output only sentences 1 to 5";
 }
 
     try {
-      const response = await fetch('/.netlify/functions/getQuizQuestions', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ content: userContent }) // Sending content to the serverless function
-      });
+const OPENAI_API_ENDPOINT = 'https://api.openai.com/v1/chat/completions';
+const payload = {
+    model: "gpt-3.5-turbo-0613",
+    messages: [
+        { "role": "system", "content": "You are a Spanish teacher." },
+        { "role": "user", "content": userContent }
+    ],
+    max_tokens: 300
+};
 
-      const data = await response.json();
+const response = await axios.post(OPENAI_API_ENDPOINT, payload, {
+    headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
+    }
+});
+
+
+      const data = response.data;
+
       setQuizData(data);
       setLoading(false);
+      setShowAnswers(false);
+
     } catch (error) {
       console.error("Error fetching quiz data:", error);
       setLoading(false);
+      setShowAnswers(false);
+
     }
   };
 
@@ -60,24 +79,19 @@ const checkAnswers = () => {
     return;
   }
 
-  // Split based on the second occurrence of "1."
-  let firstIndex = quizData.choices[0].message.content.indexOf('1.');
-  let secondIndex = quizData.choices[0].message.content.indexOf('1.', firstIndex + 1);
+  const lines = quizData.choices[0].message.content.split('\n');
 
-  if (secondIndex === -1) {
-    console.error('Unexpected data format');
-    return;
-  }
+  lines.forEach((line, index) => {
+    let match = pattern_with_prefix.exec(line);
+    if (!match) return;
 
-  const contentAfterSolutions = quizData.choices[0].message.content.substring(secondIndex);
-  const answers = contentAfterSolutions.split('\n');
+    let answer = match[4];  // Extracting the answer from the matched groups
 
-  answers.forEach((answer, index) => {
-    let formattedAnswer = answer.replace(/^\d+\.\s*/, '');
     let inputElement = document.getElementById(`input-${index}`);
     let userInput = inputElement ? inputElement.value : null;
 
-    if (userInput === formattedAnswer) {
+    if (userInput.toLowerCase() === answer.toLowerCase()) {
+
       newFeedback[index] = "correct";
     } else {
       newFeedback[index] = "wrong";
@@ -89,55 +103,50 @@ const checkAnswers = () => {
 
 
 
+
 const formatQuestions = (data) => {
   let renderedQuestions = [];
 
   if (data && data.choices && data.choices[0] && data.choices[0].message) {
-    // Finding the second occurrence of "1."
-    let firstIndex = data.choices[0].message.content.indexOf('1.');
-    let secondIndex = data.choices[0].message.content.indexOf('1.', firstIndex + 1);
+    let content = data.choices[0].message.content;
+
+    const questionAnswerPairs = content.split('\n');
     
-    if (secondIndex === -1) {
-      console.error('Unexpected data format');
-      return;
-    }
+    questionAnswerPairs.forEach((pair, index) => {
+      // Extract the prefix, question, and answer from the pair using the updated regex pattern
+      let match = pattern_with_prefix.exec(pair);
+      if (!match) return;
 
-    const contentBeforeSolutions = data.choices[0].message.content.substring(firstIndex, secondIndex).trim();
-    const questions = contentBeforeSolutions.split('\n');
-
-    const contentAfterSolutions = data.choices[0].message.content.substring(secondIndex);
-    const answers = contentAfterSolutions.split('\n');
-
-    questions.forEach((question, index) => {
-      // Split the question around the placeholder
-      let parts = question.split(/\((\w+)\)/g);
-
-      // If parts length is less than 3, it's not a valid question, so skip
-      if (parts.length < 3) return;
+      let prefix = match[1].trim();
+      let verb = match[2];
+      let questionContent = match[3].trim();
+      let answer = match[4];
 
       let feedbackElement = null;
       if (feedback[index]) {
         feedbackElement = <span className={`feedback ${feedback[index]}`}>{feedback[index]}</span>;
       }
 
-      let answerText = null;
-      if (showAnswers && answers[index]) {
-        let formattedAnswer = answers[index].replace(/^\d+\.\s*/, '');
-        answerText = <span>{formattedAnswer}</span>;
-      }
+let answerText = null;
+if (showAnswers) {
+    answerText = <span>{answer}</span>;
+}
 
-      renderedQuestions.push(
-        <p key={index}>
-          {parts[0]} 
-          <input id={`input-${index}`} placeholder={parts[1]} /> 
-          {parts[2]} {answerText} {feedbackElement}
-        </p>
-      );
+renderedQuestions.push(
+    <p key={index}>
+        {prefix} 
+        <input id={`input-${index}`} placeholder={verb} /> 
+        {questionContent} {answerText} {feedbackElement}
+    </p>
+);
+
     });
   }
 
   return renderedQuestions;
 };
+
+
 
 
 
@@ -176,8 +185,10 @@ return (
       <button onClick={() => setShowAnswers(!showAnswers)}>Show Answers</button>
 
       {formatQuestions(quizData)}
-
+ <div id="output"></div>
       {loading && <p>Loading...</p>}
+           {/* Display API Response for Debugging */}
+
     </div>
   );
 };
